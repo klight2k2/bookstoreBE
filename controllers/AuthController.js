aa61af4a985f7a012bd0ce3a0208b151f85bd162
klight <phamminhquang2k2@gmail.com>
2023-01-15 22:41:06 +0700

    add hash password

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -19,7 +19,8 @@ class AuthController {
 				res.status(500).json({code:500,message:"Email isn't exist!"});
 				return;
 			}
-			if(user.password === req.body.password){
+			const validPassword=bcrypt.compare(req.body.password,user.password);
+			if(validPassword){
 				const token = jwt.sign(
 					{ id: user.id,
 						email:user.email,
@@ -40,7 +41,7 @@ class AuthController {
 		const pool=req.app.locals.db
 		try {
 			await pool.request().input('email',sql.VarChar,req.body.email)
-		.query('SELECT * FROM [User] where email=@email ', function(err, recordset) {
+		.query('SELECT * FROM [User] where email=@email ', async(err, recordset)=> {
 			if (err) {
 			  console.error(err)
 			  res.status(500).send('SERVER ERROR')
@@ -55,6 +56,8 @@ class AuthController {
 			console.log(req.body.sex=='male');
 			if(req.body.sex !=='female' && req.body.sex!=='male') return res.status(500).json({message:"register failed!"});
 			 if(!req.body.role_id || req.body.role_id!=2)req.body.role_id=1;
+			const salt= await bcrypt.genSalt(10);
+			 req.body.password=await bcrypt.hash(req.body.password,salt);
 			const query=`INSERT INTO [User] ([fullname],[sex],[DOB],[email],[phone_number],[address],[password],[role_id]) VALUES
 							(N'${req.body.fullname}','${req.body.sex}','${req.body.dob}',N'${req.body.email}',N'${req.body.phone_number}',N'${req.body.address}',N'${req.body.password}',${req.body.role_id});`
 				pool.query(query,(err,result)=>{
